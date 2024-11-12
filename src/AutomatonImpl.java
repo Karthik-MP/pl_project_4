9ca4641132c3d2532f0070367e4e15d5b3f30242
Karthik-MP <karthikmp521@gmail.com>
2024-11-11 21:50:11 -0500

    part 1 code changes

--- src/AutomatonImpl.java
+++ src/AutomatonImpl.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Objects;
 
 public class AutomatonImpl implements Automaton {
@@ -23,8 +24,11 @@ public class AutomatonImpl implements Automaton {
 
     HashSet<Integer> start_states;
     HashSet<Integer> accept_states;
-    HashSet<Integer> current_states;
+    HashSet<Integer> current_states = new HashSet<>();;
     HashMap<StateLabelPair, HashSet<Integer>> transitions;
+
+    private HashSet<Integer> all_states = new HashSet<>();
+    private Map<Integer, Boolean> stateAcceptanceMap = new HashMap<>();
 
     public AutomatonImpl() {
         start_states = new HashSet<Integer>();
@@ -34,38 +38,67 @@ public class AutomatonImpl implements Automaton {
 
     @Override
     public void addState(int s, boolean is_start, boolean is_accept) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        all_states.add(s);
+        stateAcceptanceMap.put(s, is_accept);
+        if (is_start) {
+            current_states.add(s);  
+        }
     }
 
     @Override
     public void addTransition(int s_initial, char label, int s_final) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
+        StateLabelPair pair = new StateLabelPair(s_initial, label);
+        transitions.computeIfAbsent(pair, k -> new HashSet<>()).add(s_final);
     }
 
     @Override
     public void reset() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+        current_states.clear();
+        for (int state : all_states) {
+            if (stateAcceptanceMap.get(state) == false) {  
+                current_states.add(state);
+            }
+        }
     }
 
     @Override
     public void apply(char input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+        HashSet<Integer> next_states = new HashSet<>();
+
+        for (int state : current_states) {
+            StateLabelPair pair = new StateLabelPair(state, input);
+            HashSet<Integer> possible_next_states = transitions.get(pair);
+            if (possible_next_states != null) {
+                next_states.addAll(possible_next_states);
+            }
+        }
+
+        if (!next_states.isEmpty()) {
+            current_states = next_states;
+        } else {
+            current_states.clear();
+        }
     }
 
     @Override
     public boolean accepts() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+        for (int state : current_states) {
+            if (stateAcceptanceMap.getOrDefault(state, false)) {
+                return true;  
+            }
+        }
+        return false;  
     }
 
     @Override
     public boolean hasTransitions(char label) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hasTransitions'");
+        for (int state : current_states) {
+            StateLabelPair pair = new StateLabelPair(state, label);
+            if (transitions.containsKey(pair)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
