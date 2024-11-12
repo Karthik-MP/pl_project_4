d26a36d5217e72f0987c88ec42b7cd7808cdf6a6
Karthik-MP <karthikmp521@gmail.com>
2024-11-12 15:48:05 -0500

    Code changes for part 2

--- src/ParserImpl.java
+++ src/ParserImpl.java
@@ -17,8 +17,56 @@ public class ParserImpl extends Parser {
      */
     @Override
     public Expr do_parse() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'do_parse'");
+        return T(); 
     }
 
+    private Expr T() throws Exception {
+        Expr left = F();  
+
+        if (peek(TokenType.PLUS, 0) || peek(TokenType.MINUS, 0)) {
+            Token operator = consume(peek(TokenType.PLUS, 0) ? TokenType.PLUS : TokenType.MINUS); 
+            Expr right = T(); 
+            if (operator.ty == TokenType.PLUS) {
+                return new PlusExpr(left, right); 
+            } else {
+                return new MinusExpr(left, right); 
+            }
+        }
+
+        return left; 
+    }
+
+    private Expr F() throws Exception {
+        Expr left = Lit();  
+        
+        if (peek(TokenType.TIMES, 0) || peek(TokenType.DIV, 0)) {
+            Token operator = consume(peek(TokenType.TIMES, 0) ? TokenType.TIMES : TokenType.DIV); 
+            Expr right = F(); 
+            if (operator.ty == TokenType.TIMES) {
+                return new TimesExpr(left, right);
+            } else {
+                return new DivExpr(left, right); 
+            }
+        }
+
+        return left;  
+    }
+
+    private Expr Lit() throws Exception {
+        if (peek(TokenType.NUM, 0)) {
+            Token token = consume(TokenType.NUM);  
+            return new FloatExpr(Float.parseFloat(token.lexeme));  
+        } else if (peek(TokenType.LPAREN, 0)) {
+            consume(TokenType.LPAREN);  
+            Expr expr = T(); 
+            if (peek(TokenType.RPAREN, 0)) {
+                consume(TokenType.RPAREN); 
+                return expr; 
+            } else {
+                throw new Exception("Expected closing parenthesis RPAREN");
+            }
+        } else {
+            throw new Exception("Expected NUM or LPAREN");
+        }
+    }
 }
